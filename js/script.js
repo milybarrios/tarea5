b8d565861884169bddb01e208d64f97c6872a3c3
Your Name <you@example.com>
2016-09-19 13:54:45 -0400

    tarea5

---
+++ js/script.js
@@ -0,0 +1,16 @@
+(function () {
+
+  console.log('Hola mundo'); 
+
+
+
+}());
+var miarray = []; 
+var letra = 'MiString';
+array.push("elemento 2"); 
+array.push("elemento 1"); 
+console.log(miarray);
+var miObjeto = {}; 
+miObjeto.descripcion = "mesa"; 
+miObjeto.cantidad = 12; 
+console.log(miObjeto);
